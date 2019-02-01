9002f1b8eb42bbb0c25807e0fae0ab6d187ad05f
arcanrun <arcan.run@gmail.com>
2019-02-01 13:50:49 +0300

    Updates

---
+++ precache-manifest.c9e98c6d91777752686329001200e112.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "14c32a6dd011b2992236d9be20bf844a",
+    "url": "/vk-app-photoLike/static/media/menu.14c32a6d.svg"
+  },
+  {
+    "revision": "c4cfb5ca54dcf9fa04a387f476d4e193",
+    "url": "/vk-app-photoLike/static/media/heart_red.c4cfb5ca.svg"
+  },
+  {
+    "revision": "6c06f3e72f063fab5b5eaa430aa93cec",
+    "url": "/vk-app-photoLike/static/media/heart.6c06f3e7.svg"
+  },
+  {
+    "revision": "bc4634b50b5ff9d94c9ac8b8e90e00cc",
+    "url": "/vk-app-photoLike/static/media/close.bc4634b5.svg"
+  },
+  {
+    "revision": "2d942223eccba9af2e62e0f955671d29",
+    "url": "/vk-app-photoLike/static/media/Lobster.2d942223.ttf"
+  },
+  {
+    "revision": "fa82b39025a9d8067706",
+    "url": "/vk-app-photoLike/static/js/runtime~main.fa82b390.js"
+  },
+  {
+    "revision": "ace836977130ca394b3c",
+    "url": "/vk-app-photoLike/static/js/main.ace83697.chunk.js"
+  },
+  {
+    "revision": "ed574fef66e493db3ec5",
+    "url": "/vk-app-photoLike/static/js/1.ed574fef.chunk.js"
+  },
+  {
+    "revision": "ace836977130ca394b3c",
+    "url": "/vk-app-photoLike/static/css/main.38880bc9.chunk.css"
+  },
+  {
+    "revision": "df2d59cf13ba37faa7325576904e1ccc",
+    "url": "/vk-app-photoLike/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/vk-app-photolike/precache-manifest.a5b6528d9033df1a1fad2503746c167c.js"
+  "/vk-app-photoLike/precache-manifest.c9e98c6d91777752686329001200e112.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/vk-app-photolike/index.html", {
+workbox.routing.registerNavigationRoute("/vk-app-photoLike/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
